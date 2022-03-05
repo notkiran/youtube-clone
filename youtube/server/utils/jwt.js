e5a58074891711bbfc62656a6e2e255f745a2639
Kiran G <mail@kirang.me>
2022-03-05 19:05:13 +0530

    update fix

--- youtube/server/utils/jwt.js
+++ youtube/server/utils/jwt.js
@@ -1,36 +1,27 @@
-const mongoose = require("mongoose");
-const { extractCommentInfo } = require("../../utils");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
 
-const methods = (commentSchema) => {
-  commentSchema.statics.getVideoComments = async function (videoId) {
-    const Comment = this;
-    try {
-      const comments = await Comment.find({ videoId })
-        .sort({ createdAt: -1 })
-        .populate("commentBy");
-
-      const commentResults = comments.map((comment) =>
-        extractCommentInfo(comment)
-      );
-      return commentResults;
-    } catch (err) {
-      throw err;
-    }
-  };
-
-  commentSchema.statics.createVideoComments = async function (comment) {
-    const Comment = this;
-    try {
-      const newComment = await Comment.create(comment);
-      const newCommentPopulated = await newComment
-        .populate("commentBy")
-        .execPopulate();
-      const commentResult = extractCommentInfo(newCommentPopulated);
-      return commentResult;
-    } catch (err) {
-      throw err;
-    }
-  };
+const generateToken = ({ id, errorMessage, secret }) => {
+  try {
+    const token = jwt.sign({ id }, secret, {
+      expiresIn: `${config.SESSION_DURATION * 60 * 1000}`,
+    });
+    return token;
+  } catch (err) {
+    throw errorMessage || err;
+  }
 };
 
-module.exports = methods;
+const verifyToken = ({ token, errorMessage, secret }) => {
+  try {
+    const jwToken = jwt.verify(token, secret);
+    return jwToken.id;
+  } catch (err) {
+    throw errorMessage || err;
+  }
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
